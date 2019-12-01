530220132fc65233e07b6990dd5eea2d82da484f
Kublickaitė Asta <asta.kublickaite@mif.stud.vu.lt>
2019-12-01 18:16:55 +0200

    Adding decoding files

---
+++ decoding.h
@@ -0,0 +1,19 @@
+#ifndef DECODING_H
+#define DECODING_H
+
+#include <iostream>
+#include <fstream>
+#include <bitset>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+
+#include "hufftree.h"
+
+using namespace std;
+
+class Decoding{
+    
+};
+
+#endif
